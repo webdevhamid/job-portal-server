04db3ca3bb3bc70015566aa4a5b18749818ca0f5
webdevhamid <abdulhamid1110000@gmail.com>
2025-03-15 23:00:54 +0600

    Secured and verified token with JWT

--- index.js
+++ index.js
@@ -7,52 +7,36 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 
-// Middleware
+// Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://job-portal-b841a.web.app",
+      "https://job-portal-b841a.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
-// Custom Middleware
-const logger = (req, res, next) => {
-  console.log("Inside the logger middleware");
-  next();
-};
-
 const verifyToken = (req, res, next) => {
-  // console.log("Inside verify token middleware", req.cookies);
-  const token = req?.cookies?.token;
-  //
-  if (!token) {
-    return res.status(401).send({ message: "Unauthorized access!" });
-  }
-  //
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Unauthorized Access" });
-    }
+  const token = req.cookies?.token;
 
-    next();
-  });
-};
-
-const verifyToken2 = (req, res, next) => {
-  const token = req?.cookies?.token;
   if (!token) {
     return res.status(401).send({ message: "Unauthorized access" });
   }
 
+  // Verity the token
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    // return err, if the token is invalid
     if (err) {
-      return res.status(401).send({ message: "Unauthorized access" });
+      return res.status(403).send({ message: "Unauthorized Access" });
     }
-    // If successfully verified
+    // Store the token info to the user object
     req.user = decoded;
-    //
+
     next();
   });
 };
@@ -72,30 +56,42 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // Job related API's
     const database = client.db("jobPortalDB");
     const jobsCollection = database.collection("jobs");
     const jobApplicationCollection = database.collection("jobApplications");
 
-    // API for creating jwt token
+    // Auth related APIs
     app.post("/jwt", async (req, res) => {
-      const { email } = req.body;
-      console.log({ email });
-      const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
+      const userEmail = req.body;
+
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10h" });
+
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false, // For localhost
-          // sameSite: "strict",
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
-        .send({ success: true });
+        .send({ message: "Token successfully created!" });
+    });
+
+    // Clearing cookies after successful logout
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ message: "Logged out successfully!" });
     });
 
     // GET API endpoint for accessing jobs with or without email
-    app.get("/jobs", logger, async (req, res) => {
-      console.log("Inside the other API callback");
+    app.get("/jobs", async (req, res) => {
+      // console.log("Inside the other API callback");
 
       const email = req.query.email;
 
@@ -145,7 +141,7 @@ async function run() {
         },
       };
       const newUpdatedJob = await jobsCollection.updateOne(query, updateDoc);
-      console.log(newUpdatedJob);
+      // console.log(newUpdatedJob);
 
       // Return the result as response
       res.send(result);
@@ -156,18 +152,22 @@ async function run() {
     // Get data (one, some, many);
 
     // API for getting my applied jobs using email address query
-    app.get("/job-application", verifyToken2, async (req, res) => {
+    app.get("/job-application", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
 
-      if (req.user.email !== email) {
-        return res.status(403).send({ message: "Forbidden access" });
+      // if (req.user.email !== email) {
+      //   return res.status(403).send({ message: "Forbidden access" });
+      // }
+
+      if (req.user.userEmail !== email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
 
       const result = await jobApplicationCollection.find(query).toArray();
 
-      console.log(`Cookies`, req.cookies);
-      console.log(req.user);
+      // console.log(`Cookies`, req.cookies);
+      // console.log(req.user);
 
       // Poor way to aggregate data
       for (const application of result) {
@@ -210,8 +210,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
